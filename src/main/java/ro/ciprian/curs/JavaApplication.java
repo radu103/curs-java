8febd1ad0233b6972e14a5ac2ec62bab549e8400
JuzTroll <ciprianwang@gmail.com>
2023-03-07 21:52:23 +0200

    i tried

--- src/main/java/ro/ciprian/curs/JavaApplication.java
+++ src/main/java/ro/ciprian/curs/JavaApplication.java
@@ -1,13 +1,30 @@
 package ro.ciprian.curs;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@SpringBootApplication
+@Configuration
+@EnableWebSecurity
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class JavaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaApplication.class, args);
-	}
-
+	@Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+        return http.build();
+    }
 }
