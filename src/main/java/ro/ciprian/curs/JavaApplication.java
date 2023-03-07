e29c6e84fe5d350a829c7f48021a3d08ab24e906
JuzTroll <ciprianwang@gmail.com>
2023-03-07 23:13:08 +0200

    y

--- src/main/java/ro/ciprian/curs/JavaApplication.java
+++ src/main/java/ro/ciprian/curs/JavaApplication.java
@@ -3,7 +3,10 @@ package ro.ciprian.curs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
