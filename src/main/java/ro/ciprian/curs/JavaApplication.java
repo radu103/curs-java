723662a158f098718580429d2d7be4e625f71acc
JuzTroll <ciprianwang@gmail.com>
2023-03-07 23:01:25 +0200

    da

--- src/main/java/ro/ciprian/curs/JavaApplication.java
+++ src/main/java/ro/ciprian/curs/JavaApplication.java
@@ -3,10 +3,7 @@ package ro.ciprian.curs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,16 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class JavaApplication extends WebSecurityConfigurerAdapter {
-
-	// @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    //     http.authorizeRequests()
-    //         .anyRequest()
-    //         .authenticated()
-    //         .and()
-    //         .httpBasic();
-    //     return http.build();
-    // }
 
     @Override
     protected void configure(HttpSecurity security) throws Exception
