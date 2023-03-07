dce31a65c5a73a5a4d86536365011bbf7cdc0df2
cygnushg <45734699+cygnushg@users.noreply.github.com>
2023-03-07 21:01:46 +0200

    ciprian ma troleaza

---
+++ src/main/java/ro/andu/curs/CursApplication.java
@@ -0,0 +1,13 @@
+package ro.andu.curs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/andu/curs/CursApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.andu.curs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
