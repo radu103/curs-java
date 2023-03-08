2c89f964e25eaaf59a4104318d261f6e68c48cd9
MobR10 <100953132+MobR10@users.noreply.github.com>
2023-03-08 13:00:58 +0200

    removed "actuator" and added "exclude" in run

--- src/main/java/ro/raffa/curs/JavaApplication.java
+++ src/main/java/ro/raffa/curs/JavaApplication.java
@@ -2,8 +2,9 @@ package ro.raffa.curs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class JavaApplication {
 
 	public static void main(String[] args) {
