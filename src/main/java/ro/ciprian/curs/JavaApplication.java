b3416ba62d6b4f375c134b6934f3400eda90ced2
Ciprian <ciprianwang@gmail.com>
2023-03-10 16:13:58 +0200

    added security conf

--- src/main/java/ro/ciprian/curs/JavaApplication.java
+++ src/main/java/ro/ciprian/curs/JavaApplication.java
@@ -3,25 +3,9 @@ package ro.ciprian.curs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class JavaApplication extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-     security.formLogin().disable();
-    }
-
+@SpringBootApplication
+public class JavaApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaApplication.class, args);
     }
---
+++ src/main/java/ro/ciprian/curs/configuration/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package ro.ciprian.curs.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .csrf().disable()
+            .authorizeHttpRequests((authorize) -> authorize
+            .mvcMatchers("/**").permitAll()
+            .anyRequest().permitAll());
+        return http.build();
+    }
+}
