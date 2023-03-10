837ae77e0c294823da79b1328f2e68f96f309f99
MobR10 <anghelraffaele10@gmail.com>
2023-03-10 17:32:19 +0200

    lectie 10.03.2023

---
+++ src/main/java/ro/raffa/curs/controller/v1/HelloWorldController.java
@@ -0,0 +1,46 @@
+package ro.raffa.curs.controller.v1;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/v1")
+public class HelloWorldController {
+    
+    @GetMapping(path ="/helloworld/hello1")
+    public String getHello1() {
+        return "Hello World";
+    } 
+    @PostMapping (path ="/helloworld/hello1", 
+    consumes={MediaType.APPLICATION_JSON_VALUE},
+    produces={MediaType.APPLICATION_JSON_VALUE})
+    public String postHello1(@RequestBody String input) {
+        return "Hello World Post : " + input;
+    }
+
+
+
+  /*  
+    @GetMapping 
+    public String hello2() {
+        return "BYEBYE WORLD";
+    }
+
+    //aici da eroare, pentru ca,  
+    // daca lasi doua @GetMapping-uri fara path,
+    // programul incearca sa mearga pe /v1,
+    // care e la @RequestMapping
+    // dar, fiind doua @GetMapping fara path, 
+    // ele intra in conflict si da eroare la build :)
+    
+    @GetMapping
+    public String hello3() {
+        return "Is there a conflict?";
+    }
+    
+    */ 
+}
