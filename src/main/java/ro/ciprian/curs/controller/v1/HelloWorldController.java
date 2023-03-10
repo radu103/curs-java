3cad89907018c347b9f0813706050d64f0cac9e5
Ciprian <ciprianwang@gmail.com>
2023-03-10 17:31:39 +0200

    need to finish helloworld postmapping

---
+++ src/main/java/ro/ciprian/curs/controller/v1/HelloWorldController.java
@@ -0,0 +1,23 @@
+package ro.ciprian.curs.controller.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/v1")
+public class HelloWorldController {
+    
+    @GetMapping(path = "/helloworld/hello1")
+    public String hello1(){
+        return "Hello World";
+    }
+
+    // @PostMapping(path = "/helloworld/hello1", produces = ContentType)
+    // public String postHello1(){
+    //     return "Hello World POST";
+    //TODO: de terminat asta
+    //}
+}
