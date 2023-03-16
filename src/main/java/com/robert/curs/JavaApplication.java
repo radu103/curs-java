83cd6f82b93ac4aea5145dc6f23dfc1429d12890
robert <rnechita06@gmail.com>
2023-03-16 22:30:47 +0200

    poate mere

--- src/main/java/com/robert/curs/JavaApplication.java
+++ src/main/java/com/robert/curs/JavaApplication.java
@@ -2,26 +2,12 @@ package com.robert.curs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class JavaApplication extends WebSecurityConfigurerAdapter {
+@SpringBootApplication
+public class JavaApplication {
 
-    @Override
-    protected void configure(HttpSecurity security) throws Exception
-    {
-     security.formLogin().disable();
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(JavaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApplication.class, args);
+	}
+	//test
 }
