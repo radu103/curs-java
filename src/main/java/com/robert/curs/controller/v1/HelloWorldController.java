3d26f61277aedbe1e6e75c8df9e68d91d945092b
robert <rnechita06@gmail.com>
2023-03-16 22:40:25 +0200

    merge!!!

---
+++ src/main/java/com/robert/curs/controller/v1/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.robert.curs.controller.v1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HelloWorldController {
+    
+    @GetMapping(path = "/helloworld/hello1")
+    public String hello1() {
+        return "Hello World";
+    }
+}
