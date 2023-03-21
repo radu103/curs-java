d8fc0fa85858a43c9fa8803fa938ff3996b303ac
Ficus12 <andrei.tdr767@gmail.com>
2023-03-21 23:21:22 +0200

    updated 21.03.2023

--- src/main/java/com/andrei/curs/dto/HelloRequest.java
+++ src/main/java/com/andrei/curs/dto/HelloRequest.java
@@ -1,10 +1,15 @@
 package com.andrei.curs.dto;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class HelloRequest {
 
-    @JsonProperty("input")
-    String input;
+    @JsonProperty("a")
+    private String a;
+
+    @JsonProperty("b")
+    private String b;
+
 }
