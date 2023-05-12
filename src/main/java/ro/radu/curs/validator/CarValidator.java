bff1fb550dd2bc9b01de18e6b828946ea77e22b2
Radu Simen <radu.simen@chooose.today>
2023-05-12 16:22:29 +0300

    feat: validator

---
+++ src/main/java/ro/radu/curs/validator/CarValidator.java
@@ -0,0 +1,28 @@
+package ro.radu.curs.validator;
+
+import java.math.BigDecimal;
+
+import lombok.experimental.UtilityClass;
+import ro.radu.curs.exception.CarValidatorException;
+import ro.radu.curs.model.Car;
+
+@UtilityClass
+public class CarValidator {
+
+    public static Boolean validateCar(Car car) throws CarValidatorException {
+        return validateCarMaker(car.getMaker());
+    }
+
+    public static Boolean validateCarMaker(String name) throws CarValidatorException {
+        if (name == null || name.isEmpty()) {
+            throw new CarValidatorException(90001, "Maker is null or empty");
+        }
+        if (name.length() > 15) {
+            throw new CarValidatorException(90002, "Maker is too long");
+        }
+        if (name.toLowerCase().charAt(0) == name.charAt(0)) {
+            throw new CarValidatorException(90003, "Maker must start with uppercase");
+        }
+        return true;
+    }
+}
