4a8f565a2e1f8afe61b743d75f3e4e9deb663e88
cygnushg <anduciontea@ggmail.com>
2023-05-18 23:46:44 +0300

    tema

--- src/main/java/ro/andu/curs/validator/CarValidator.java
+++ src/main/java/ro/andu/curs/validator/CarValidator.java
@@ -1,5 +1,8 @@
 package ro.andu.curs.validator;
 
+import java.math.BigDecimal;
+import java.util.Calendar;
+
 import lombok.experimental.UtilityClass;
 import ro.andu.curs.exception.CarValidatorException;
 import ro.andu.curs.model.Car;
@@ -8,6 +11,11 @@ import ro.andu.curs.model.Car;
 public class CarValidator {
     public static Boolean validateCar(Car car) throws CarValidatorException {
         validateCarMaker(car.getMaker());
+        validateCarModel(car.getModel());
+        validateCarColor(car.getColor());
+        validateCarYear(car.getYear());
+        validateCarPrice(car.getPrice());
+        validateCarCurrency(car.getCurrency());
         return true;
     }
 
@@ -20,6 +28,56 @@ public class CarValidator {
         }
         if (name.toLowerCase().charAt(0) == (name.charAt(0))) {
             throw new CarValidatorException(90003, "Maker name first letter must be upper case");
+        }
+    }
+    public static void validateCarModel(String model) throws CarValidatorException {
+        if (model == null || model.isEmpty()) {
+            throw new CarValidatorException(90011, "Model name is null");
+        }
+        if (model.length() > 15) {
+            throw new CarValidatorException(90012, "Model name must be smaller then 15 letters");
+        }
+        if (model.toLowerCase().charAt(0) == (model.charAt(0))) {
+            throw new CarValidatorException(90013, "Model name first letter must be upper case");
+        }
+    }
+    public static void validateCarColor(String color) throws CarValidatorException {
+        if (color == null || color.isEmpty()) {
+            throw new CarValidatorException(90021, "Color is null");
+        }
+        if (color.length() > 15) {
+            throw new CarValidatorException(90022, "Color size must be smaller then 15 letters");
+        }
+        if (color.toLowerCase().charAt(0) == (color.charAt(0))) {
+            throw new CarValidatorException(90023, "Color first letter must be upper case");
+        }
+    }
+    public static void validateCarYear(int year) throws CarValidatorException {
+        int cYear = Calendar.getInstance().get(Calendar.YEAR);
+        if (year==0) {
+            throw new CarValidatorException(90031, "Year is null");
+        }
+        if (year < 1886) {
+            throw new CarValidatorException(90032, "Year must be bigger then 1886");
+        }
+        if (year > cYear+1) {
+            throw new CarValidatorException(90033, "Year is too big. Max: " + cYear + 1);
+        }
+    }
+    public static void validateCarPrice(BigDecimal price) throws CarValidatorException {
+        if (price.compareTo(new BigDecimal(0))==0) {
+            throw new CarValidatorException(90041, "Price is null");
+        }
+        if (price.compareTo(new BigDecimal(100))<0) {
+            throw new CarValidatorException(90042, "Car can't be this cheap");
+        }
+    }
+    public static void validateCarCurrency(String currency) throws CarValidatorException {
+        if (currency == null || currency.isEmpty()) {
+            throw new CarValidatorException(90051, "Currency is null");
+        }
+        if (!(currency.equals("EUR")|| currency.equals("USD"))) {
+            throw new CarValidatorException(90052, "Currency must be either EUR or USD");
         }
     }
 }
--- src/test/java/ro/andu/curs/validator/CarValidatorTest.java
+++ src/test/java/ro/andu/curs/validator/CarValidatorTest.java
@@ -3,6 +3,8 @@ package ro.andu.curs.validator;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigDecimal;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -12,9 +14,19 @@ import ro.andu.curs.model.Car;
 @SpringBootTest
 public class CarValidatorTest {
 
+    Car car = new Car();
+    void initCar() {
+        car.setMaker("Ford");
+        car.setModel("Focus");
+        car.setColor("Turqouse");
+        car.setYear(2024);
+        car.setPrice(new BigDecimal(10000));
+        car.setCurrency("EUR");
+    }
+    
     @Test
-    void testValidateCars_exception(){
-        Car car = new Car();
+    void testValidateCarsMaker_exception(){
+        initCar();
         car.setMaker(null);
         try {
             CarValidator.validateCar(car);;
@@ -23,29 +35,206 @@ public class CarValidatorTest {
         }
     }
     @Test
-    void testValidateCarsLenght_exception() {
-        Car car = new Car();
+    void testValidateCarsMakerLenght_exception() {
+        initCar();
         car.setMaker("aaaaaaaaaaaaaaaa");
         try {
-            CarValidator.validateCar(car);;
+            CarValidator.validateCar(car);
         } catch (CarValidatorException e) {
             assertEquals(90002, e.getErrorCode());
         }
     }
     @Test
-    void testValidateCarsUppercase_exception(){
-        Car car = new Car();
+    void testValidateCarsMakerUppercase_exception(){
+        initCar();
         car.setMaker("name");
         try {
-            CarValidator.validateCar(car);;
+            CarValidator.validateCar(car);
         } catch (CarValidatorException e) {
             assertEquals(90003, e.getErrorCode());
         }
     }
     @Test
-    void testValidateCarsIsOk_exception(){
-        Car car = new Car();
+    void testValidateCarsMakerIsOk_exception(){
+        initCar();
         car.setMaker("Ford");
         assertTrue(CarValidator.validateCar(car));
     }
+
+    //---------------------------------
+
+    @Test
+    void testValidateCarsModel_exception(){
+        initCar();
+        car.setModel(null);
+        try {
+            CarValidator.validateCar(car);;
+        } catch (CarValidatorException e) {
+            assertEquals(90011, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsModelLenght_exception() {
+        initCar();
+        car.setModel("aaaaaaaaaaaaaaaa");
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90012, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsModelUppercase_exception(){
+        initCar();
+        car.setModel("name");
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90013, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsModelIsOk_exception(){
+        initCar();
+        car.setModel("Focus");
+        assertTrue(CarValidator.validateCar(car));
+    }
+
+    //---------------------------------
+
+    @Test
+    void testValidateCarsColor_exception(){
+        initCar();
+        car.setColor(null);
+        try {
+            CarValidator.validateCar(car);;
+        } catch (CarValidatorException e) {
+            assertEquals(90021, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsColorLenght_exception() {
+        initCar();
+        car.setColor("aaaaaaaaaaaaaaaa");
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90022, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsColorUppercase_exception(){
+        initCar();
+        car.setColor("name");
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90023, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsColorIsOk_exception(){
+        initCar();
+        car.setColor("Turqouse");
+        assertTrue(CarValidator.validateCar(car));
+    }
+
+    //---------------------------------
+
+    @Test
+    void testValidateCarsYear_exception(){
+        initCar();
+        car.setYear(0);
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90031, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsYearOlder_exception() {
+        initCar();
+        car.setYear(1800);
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90032, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsYearNewer_exception(){
+        initCar();
+        car.setYear(2050);
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90033, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsYearIsOk_exception(){
+        initCar();
+        car.setYear(2024);
+        assertTrue(CarValidator.validateCar(car));
+    }
+
+    //---------------------------------
+
+    @Test
+    void testValidateCarsPrice_exception(){
+        initCar();
+        car.setPrice(new BigDecimal(0));
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90041, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsPriceExists_exception() {
+        initCar();
+        car.setPrice(new BigDecimal(1));
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90042, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsPriceIsOk_exception(){
+        initCar();
+        car.setPrice(new BigDecimal(10000));
+        assertTrue(CarValidator.validateCar(car));
+    }
+
+    //---------------------------------
+
+    @Test
+    void testValidateCarsCurrency_exception(){
+        initCar();
+        car.setCurrency(null);
+        try {
+            CarValidator.validateCar(car);;
+        } catch (CarValidatorException e) {
+            assertEquals(90051, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsCurrencyValidity_exception() {
+        initCar();
+        car.setCurrency("aaaaaaaaaaaaaaaa");
+        try {
+            CarValidator.validateCar(car);
+        } catch (CarValidatorException e) {
+            assertEquals(90052, e.getErrorCode());
+        }
+    }
+    @Test
+    void testValidateCarsCurrencyIsOk_exception(){
+        initCar();
+        car.setCurrency("EUR");
+        assertTrue(CarValidator.validateCar(car));
+    }
+
+    //---------------------------------
 }
