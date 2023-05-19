33ceb476ae3f9869bfb85844b6e22e007f31e464
robert <rnechita06@gmail.com>
2023-05-19 11:03:04 +0300

    database

--- src/main/java/ro/radu/curs/model/Car.java
+++ src/main/java/ro/radu/curs/model/Car.java
@@ -2,16 +2,46 @@ package ro.radu.curs.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 
 @Data
-public class Car {
+@Entity
+@Table(name = "cars", indexes = @Index(name = "cars_yyears_idx", columnList = "year"))
+public class Car{
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+    @Column(name = "maker")
     private String maker;
+
+    @Column(name = "model")
     private String model;
+
+    @Column(name = "color")
     private String color;
+    
+    @Column(name = "year")
     private Integer year;
+
+    @Column(name = "price")
     private BigDecimal price;
+
+    @Column(name = "currency")
     private String currency;
+
+    @Column(name = "Hp")
     private Integer Hp;
 }
