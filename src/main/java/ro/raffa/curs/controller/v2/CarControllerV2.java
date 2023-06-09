53862eace0c9ca59bdd6fb1803e08f09ce24e992
MobR10 <anghelraffaele10@gmail.com>
2023-06-09 12:52:54 +0300

    lectie 9.06

--- src/main/java/ro/raffa/curs/controller/v2/CarControllerV2.java
+++ src/main/java/ro/raffa/curs/controller/v2/CarControllerV2.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ro.raffa.curs.dto.CarDto;
+import ro.raffa.curs.mapper.CarMapper;
 import ro.raffa.curs.model.Car;
 import ro.raffa.curs.repository.DbCarRepository;
 
@@ -18,6 +22,9 @@ public class CarControllerV2 {
 
     @Autowired
     DbCarRepository carRepository;
+
+    @Autowired
+    CarMapper mapper;
     
     @GetMapping("/car/list")
     public List<Car> getCars() {
@@ -27,6 +34,13 @@ public class CarControllerV2 {
 
     @PostMapping("/car/add")
     public Car addCar(@RequestBody Car car) {
+        return carRepository.save(car);
+    }
+
+    @PostMapping("/car/update/{id}")
+    public Car addCar(@RequestBody CarDto carDto, @PathVariable Long id) {
+        carDto.setId(id);
+        Car car = mapper.toEntity(carDto);
         return carRepository.save(car);
     }
 
--- src/main/java/ro/raffa/curs/dto/CarDto.java
+++ src/main/java/ro/raffa/curs/dto/CarDto.java
@@ -7,6 +7,7 @@ import lombok.Data;
  */
 @Data
 public class CarDto {
+    private Long id;
     private String maker;
     private String model;
     private String color;
