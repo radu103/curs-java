5e2d5b2ff3026c67c5efeb19b7f9f49dc22d4302
robert <rnechita06@gmail.com>
2023-06-09 10:38:55 +0300

    daa

--- src/main/java/ro/radu/curs/controller/v2/CarControllerV2.java
+++ src/main/java/ro/radu/curs/controller/v2/CarControllerV2.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ro.radu.curs.model.Car;
@@ -30,4 +32,16 @@ public class CarControllerV2 {
         return carRepository.save(car);
     }
     
+    @PostMapping("/car/update/{id}")
+    public Car updateById(@RequestBody Car car, @PathVariable Integer id){
+        Long id2 = Long.valueOf(id);
+        Car car1 = carRepository.findById(id2).get();
+        car1.setColor(car.getColor());
+        car1.setCurrency(car.getCurrency());
+        car1.setMaker(car.getMaker());
+        car1.setModel(car.getModel());
+        car1.setPrice(car.getPrice());
+        car1.setYear(car.getYear());
+        return carRepository.save(car1);
+    }
 }
